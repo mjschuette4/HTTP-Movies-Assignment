02def02cef9d2b07fe389debcdca0cea17fca53e
MatthewSchuette <mjschuette4@gmail.com>
2020-04-22 17:05:24 -0500

    MVP?

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateMovie from "./actions/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -12,7 +13,10 @@ const App = () => {
   const getMovieList = () => {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => setMovieList(res.data))
+      .then(res => {
+        console.log("here be res", res);
+        setMovieList(res.data);
+      })
       .catch(err => console.log(err.response));
   };
 
@@ -32,9 +36,18 @@ const App = () => {
         <MovieList movies={movieList} />
       </Route>
 
-      <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
-      </Route>
+      <Route
+        path="/movies/:id"
+        render={props => <Movie addToSavedList={addToSavedList} {...props} />}
+      />
+
+      <Route
+        exact
+        path="/update-movie/:id"
+        render={props => (
+          <UpdateMovie {...props} addToSavedList={addToSavedList} />
+        )}
+      />
     </>
   );
 };
